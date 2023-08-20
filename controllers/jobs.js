d50f26ef0212278cfe8bd9c0f8cdb2f6be6fbfd4
willy836 <wilsonkimiri836@gmail.com>
2023-08-20 17:03:52 +0300

    Complete general setup

---
+++ controllers/jobs.js
@@ -0,0 +1,21 @@
+const getAllJobs = async (req, res) => {
+  res.send("Get all jobs");
+};
+
+const getJob = async (req, res) => {
+  res.send("Get single job");
+};
+
+const createJob = async (req, res) => {
+  res.send("Create job");
+};
+
+const updateJob = async (req, res) => {
+  res.send("Update job");
+};
+
+const deleteJob = async (req, res) => {
+  res.send("Delete job");
+};
+
+module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
