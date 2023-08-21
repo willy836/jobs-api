a83d5bd5194ebdea50a3e00461f5d0f0274759ba
willy836 <wilsonkimiri836@gmail.com>
2023-08-21 08:55:23 +0300

    Lgin User

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body }); // database query is async
@@ -8,7 +9,25 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("Login user");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = { register, login };
